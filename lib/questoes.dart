f3d00c4faa32be74437e871aa2bba58d880ec1e8
João Vitor Ribeiro <78767127+jvsribeiro@users.noreply.github.com>
2026-09-01 21:59:20 -0300

    Add files via upload

---
+++ lib/questoes.dart
@@ -0,0 +1,10 @@
+import 'package:tabuada/pergunta.dart';
+
+const questoes = [
+  Pergunta('Qual é a capital do Brasil?',
+      ['Brasília', 'São Paulo', 'Rio de Janeiro', 'Salvador']),
+  Pergunta('Qual é o maior país da América do Sul?',
+      ['Brasil', 'Argentina', 'Chile', 'Peru']),
+  Pergunta('Quem proclamou a Independência do Brasil?',
+      ['Dom Pedro I', 'Tiradentes', 'Dom Pedro II', 'Getúlio Vargas']),
+];
